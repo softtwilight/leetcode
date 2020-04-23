72e9f44656d958d01d4f94731fcaaf6aad147c24
softtwilight <tjuyxl@gmail.com>
2020-04-23 22:08:28 +0900

    数独，back-trace，更新三种方法

--- src/main/java/hashmap/_37_Sudoku_Solver.java
+++ src/main/java/hashmap/_37_Sudoku_Solver.java
@@ -1,6 +1,5 @@
 package hashmap;
 
-import java.sql.SQLOutput;
 import java.util.*;
 
 /**
@@ -36,14 +35,13 @@ public class _37_Sudoku_Solver {
                 {'.','.','.','8','.','3','.','2','.'},
                 {'.','.','.','.','.','.','.','.','6'},
                 {'.','.','.','2','7','5','9','.','.'}};
-        instance.solveSudoku(input);
+        instance.solveSudoku3(input);
         System.out.println(Arrays.deepToString(input));
     }
 
     private Map<Integer, Integer> rowMap = new HashMap<>();
     private Map<Integer, Integer> colMap = new HashMap<>();
     private Map<Integer, Integer> blockMap = new HashMap<>();
-    private Map<Integer, Integer> bitMap = new HashMap<>();
 
 
     /**
@@ -59,6 +57,13 @@ public class _37_Sudoku_Solver {
      *  需要引入冲突检测机制，同时为了能回退回另一种可能，board 和 map能不能被修改，需要copy一份给尝试算法。
      *  艰难的完成了一版  17 ms  +	39.1 MB
      *
+     *  不用copy，而是让map重新设置值的方式实现了一版  14 ms + 40.1 MB
+     *
+     *  利用set实现一版本不知道会怎样？
+     *  实现了一版，28 ms	+ 39.5 MB, 更慢了， 这里取并集的时间超过了循环判断isValid的时间。
+     *
+     *  所以在这个思路里，三种方法里，用二进制来判断的解法是最快的。但是并不明显。
+     *  核心的思想是尽量从可选值更少的地方开始试错。
      *
      */
     public void solveSudoku(char[][] board) {
@@ -66,7 +71,6 @@ public class _37_Sudoku_Solver {
             rowMap.put(i, 0);
             colMap.put(i, 0);
             blockMap.put(i, 0);
-//            bitMap.put(1 << i, i);
         }
 
         for (int r = 0; r < 9; r++) {
@@ -89,39 +93,37 @@ public class _37_Sudoku_Solver {
         for (int r = 0; r < 9; r++) {
             for (int col = 0; col < 9; col++) {
                 if (board[r][col] == '.') {
-                    int blockIndex = (r / 3) * 3 + col / 3;
+                    int bi = (r / 3) * 3 + col / 3;
                     int i = rowMap.get(r);
                     int j = colMap.get(col);
-                    int k = blockMap.get(blockIndex);
-                    int sum = (i | j | k) ^ 511 ;
-                    List<Integer> li = new ArrayList<>(9);
+                    int k = blockMap.get(bi);
+                    if ((i | j | k) >= 511) return false;
+                    int sum = (i | j | k) ^ 511;
+                    List<Integer> li = new ArrayList<>(3);
                     for (int x = 0; x < 9; x++) {
                         int temp = sum;
                         boolean has = ((temp >> x) & 1) == 1;
-                        if (has) {
-                            li.add(x);
-                        }
+                        if (has) li.add(x);
+                        if (li.size() == 3) break;
                     }
 
-                    if (li.size() > 0) {
-                        System.out.println(li);
+                    if (li.size() > 0 && li.size() <= 2) {
                         for (int candidate : li) {
-                            char c = (char) (candidate + 49);
-                            if (!isValid(board, r, col, c)) {
-                                continue;
-                            }
                             board[r][col] = (char) (candidate + 49);
-//                            rowMap.put(r, rowMap.get(r) + 1 << candidate);
-//                            colMap.put(col, colMap.get(col) + (1 << candidate));
-//                            blockMap.put(blockIndex, blockMap.get(blockIndex) + (1 << candidate));
+                            int s = 1 << candidate;
+                            boolean notInr = (rowMap.get(r) & s) == 0;
+                            boolean notInc = (colMap.get(col) & s) == 0;
+                            boolean notInb = (blockMap.get(bi) & s) == 0;
+                            if (notInr) rowMap.put(r, rowMap.get(r) + s);
+                            if (notInc) colMap.put(col, colMap.get(col) + s);
+                            if (notInb) blockMap.put(bi, blockMap.get(bi) + s);
                             if (solveWithMap(board)) {
                                 return true;
                             }
                             board[r][col] = '.';
-//                            rowMap.put(r, rowMap.get(r) - 1 << candidate);
-//                            colMap.put(col, colMap.get(col) - (1 << candidate));
-//                            blockMap.put(blockIndex, blockMap.get(blockIndex) - (1 << candidate));
-
+                            if (notInr) rowMap.put(r, rowMap.get(r) - s);
+                            if (notInc) colMap.put(col, colMap.get(col) - s);
+                            if (notInb) blockMap.put(bi, blockMap.get(bi) - s);
                         }
                         return false;
                     }
@@ -137,14 +139,14 @@ public class _37_Sudoku_Solver {
      * 不使用map，直接在board上验证合法与否。
      *
      */
-    private boolean solve(char[][] board) {
+    private boolean solveSudoku2(char[][] board) {
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 if (board[i][j] == '.') {
                     for (char k = '1'; k <= '9'; k++) {
                         if (isValid(board, i, j, k)) {
                             board[i][j] = k;
-                            if (solve(board)) {
+                            if (solveSudoku2(board)) {
                                 return true;
                             }
                             board[i][j] = '.';
@@ -165,6 +167,77 @@ public class _37_Sudoku_Solver {
             boolean c = board[(i / 3) * 3 + n / 3][(j / 3) * 3 + n % 3] == k;
             if (a || b || c) {
                 return false;
+            }
+        }
+        return true;
+    }
+
+    private Map<Integer, Set<Character>> rMap = new HashMap<>();
+    private Map<Integer, Set<Character>> cMap = new HashMap<>();
+    private Map<Integer, Set<Character>> bMap = new HashMap<>();
+
+    public void solveSudoku3(char[][] board) {
+        Set<Character> s = new HashSet<>();
+        for (char i = '1'; i <= '9'; i++) {
+            s.add(i);
+        }
+        for (int i = 0; i < 9; i++) {
+            rMap.put(i, new HashSet<>(s));
+            cMap.put(i, new HashSet<>(s));
+            bMap.put(i, new HashSet<>(s));
+        }
+        for (int r = 0; r < 9; r++) {
+            for (int col = 0; col < 9; col++) {
+                char c = board[r][col];
+                if (c != '.') {
+                    rMap.get(r).remove(c);
+                    cMap.get(col).remove(c);
+                    int blockIndex = (r / 3) * 3 + col / 3;
+                    bMap.get(blockIndex).remove(c);
+                }
+            }
+        }
+        System.out.println(rMap);
+        System.out.println(cMap);
+        System.out.println(bMap);
+        solveWithMap2(board);
+    }
+
+    private boolean solveWithMap2(char[][] board) {
+        for (int r = 0; r < 9; r++) {
+            for (int col = 0; col < 9; col++) {
+                char c = board[r][col];
+                if (c == '.') {
+                    int bi = (r / 3) * 3 + col / 3;
+                    Set<Character> rs = rMap.get(r);
+                    Set<Character> cs = cMap.get(col);
+                    Set<Character> bs = bMap.get(bi);
+
+                    Set<Character> inner = new HashSet<>(rs);
+                    inner.retainAll(cs);
+                    inner.retainAll(bs);
+                    if (inner.isEmpty()) return false;
+
+                    if (inner.size() < 3) {
+                        for (char candidate : inner) {
+                            board[r][col] = candidate;
+                            boolean a = rs.contains(candidate);
+                            boolean b = cs.contains(candidate);
+                            boolean bf = bs.contains(candidate);
+                            if (a) rs.remove(candidate);
+                            if (b) cs.remove(candidate);
+                            if (bf) bs.remove(candidate);
+                            if (solveWithMap2(board)) {
+                                return true;
+                            }
+                            board[r][col] = '.';
+                            if (a) rs.add(candidate);
+                            if (b) cs.add(candidate);
+                            if (bf) bs.add(candidate);
+                        }
+                        return false;
+                    }
+                }
             }
         }
         return true;
