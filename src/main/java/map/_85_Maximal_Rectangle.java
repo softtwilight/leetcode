3a1233695a5da97a19a1606b31afa6fdfaf3620f
softtwilight <tjuyxl@gmail.com>
2020-04-26 22:45:59 +0900

    添加思路

--- src/main/java/map/_85_Maximal_Rectangle.java
+++ src/main/java/map/_85_Maximal_Rectangle.java
@@ -36,15 +36,7 @@ public class _85_Maximal_Rectangle {
         System.out.println(instance.maximalRectangle(input));
     }
 
-    /**
-     * 用了最暴力的解法算的，103 ms	+ 43.4 MB
-     * 对每一个点遍历，如果是1，那么就一直往下找，直到遇到0。
-     * 然后接着往右边移动，往下找的时候，传递两个column坐标，1开始的坐标，1结束的坐标。
-     * 这样就可以保证遍历所以情况了。
-     *
-     * 这个过程会有很多重复的搜索。明天再优化吧；
-     *
-     */
+
     public int maximalRectangle(char[][] matrix) {
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return 0;
         return helper2(matrix);
@@ -57,6 +49,17 @@ public class _85_Maximal_Rectangle {
      * 我们读一行，然后0对应高度0， 1对应高度1， 计算最大矩形。
      * 然后接着读下一行，如果为0， 那么高度重置为0， 如果为1， 那么高度在原来的基础上加一
      * 然后每一行计算maxRectangle.  	7 ms  +	42.5 MB
+     *
+     * 这就是问题分解的力量，最初拿到这道题的时候真是焦头烂额。
+     * 第一个版本的思维负担其实非常重，试了二三版才写对。
+     * 而这个版本因为思路清晰了，导致实现也很容易，一次就通过了。
+     *
+     * 可是怎么去分解这个问题呢。我最开始试图以点为思考单位去遍历，然后考虑两个移动方向
+     * 向右和向下，然后实现的就是我们的第一版。可是在做的过程中，会发现很难去优化点的移动，
+     * 即使你已经知道某些点的遍历是多余的，但是向下，向右，要保存的状态等等让思考非常艰难。
+     *
+     * 可是当我们换一种抽象，以行为单位遍历，这个过程我们减少了向右遍历的过程。
+     * 我们把思考的层次变得更分明一些了。但是我不确定这是不是又必然性，或者这是一个可以尝试的思路。
      *
      */
     public int helper2(char[][] matrix) {
@@ -104,6 +107,15 @@ public class _85_Maximal_Rectangle {
         return maxArea;
     }
 
+    /**
+     * 用了最暴力的解法算的，103 ms	+ 43.4 MB
+     * 对每一个点遍历，如果是1，那么就一直往下找，直到遇到0。
+     * 然后接着往右边移动，往下找的时候，传递两个column坐标，1开始的坐标，1结束的坐标。
+     * 这样就可以保证遍历所以情况了。
+     *
+     * 这个过程会有很多重复的搜索。明天再优化吧；
+     *
+     */
     public int helper(char[][] matrix) {
         int result = 0;
         for (int i = 0; i < matrix.length; i++) {
@@ -118,7 +130,6 @@ public class _85_Maximal_Rectangle {
                         int temp = lookDown(matrix, i, loj, hij);
                         if (temp > result) {
                             result = temp;
-//                            j = hij;
                         }
                         hij++;
                     }
