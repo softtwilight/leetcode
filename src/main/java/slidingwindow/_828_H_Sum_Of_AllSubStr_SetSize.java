f558de6be02e6a637ee14ec4f99cb6d2f540decb
softtwilight <tjuyxl@gmail.com>
2020-05-15 23:05:18 +0900

    动态规划版

--- src/main/java/slidingwindow/_828_H_Sum_Of_AllSubStr_SetSize.java
+++ src/main/java/slidingwindow/_828_H_Sum_Of_AllSubStr_SetSize.java
@@ -1,5 +1,7 @@
 package slidingwindow;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode.com/problems/count-unique-characters-of-all-substrings-of-a-given-string/
  *
@@ -59,5 +61,25 @@ public class _828_H_Sum_Of_AllSubStr_SetSize {
         }
         return result;
 
+    }
+
+
+    /**
+     * TODO 动归解法， 先pass，回头再回来理解
+     */
+    public int uniqueLetterString2(String s) {
+        int[] lastPos = new int[26];
+        Arrays.fill(lastPos, -1);
+        int[] contribution = new int[26];
+        int ch = 0,  ans = 0, preSum = 0;
+
+        for(int i = 0; i < s.length(); i++){
+            ch = s.charAt(i)-'A';
+            preSum += i - lastPos[ch] - contribution[ch];
+            ans += preSum;
+            contribution[ch] = i - lastPos[ch];
+            lastPos[ch] = i;
+        }
+        return ans;
     }
 }
