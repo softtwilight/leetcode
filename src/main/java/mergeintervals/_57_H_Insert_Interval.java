1495adfca8b2c62678e800678f28a7bd1052f7aa
softtwilight <tjuyxl@gmail.com>
2020-05-21 22:14:16 +0900

    insert interval

---
+++ src/main/java/mergeintervals/_57_H_Insert_Interval.java
@@ -0,0 +1,43 @@
+package mergeintervals;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Author:   softtwilight
+ * Date:     2020/05/21 22:04
+ */
+public class _57_H_Insert_Interval {
+    private static final _57_H_Insert_Interval instance = new _57_H_Insert_Interval();
+
+    public static void main(String[] args) {
+
+        int[][] intervals = {{1,2},{3,5},{6,7},{8,10},{12,16}};
+        int[] ni = {4, 8};
+        System.out.println(Arrays.deepToString(instance.insert(intervals, ni)));
+    }
+
+    /**
+     * 	1 ms  +	41.7 MB
+     * 	这道题做过了，关键是换newInterval，就可以将比较需要考虑的情况降下来。
+     * 	掌握这个技巧就不难了。
+     *
+     */
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> result = new LinkedList<>();
+        for (int[] cur : intervals) {
+            if (cur[1] < newInterval[0]) {
+                result.add(cur);
+            } else if (cur[0] > newInterval[1]) {
+                result.add(newInterval);
+                newInterval = cur;
+            } else {
+                newInterval[0] = Math.min(newInterval[0], cur[0]);
+                newInterval[1] = Math.max(newInterval[1], cur[1]);
+            }
+        }
+        result.add(newInterval);
+        return result.toArray(new int[result.size()][2]);
+    }
+}
