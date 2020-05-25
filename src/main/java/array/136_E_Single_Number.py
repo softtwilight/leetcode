4b43cbf3267fba9d5f4e42cc3887e1a7ff7893f4
softtwilight <tjuyxl@gmail.com>
2020-05-25 23:06:34 +0900

    delete the python files

--- src/main/java/array/136_E_Single_Number.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        result = 0
-        for i in nums:
-            result ^= i
-        return result
--- src/main/java/mergeintervals/_452_M_Mininum_Arrows.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def findMinArrowShots(self, points: List[List[int]]) -> int:
-        res, arrow = 0, -float('inf')
-        points = sorted(points, key = lambda p:p[1])
-        for start, end in points:
-            if start > arrow: 
-                res += 1
-                arrow = end
-        return res 
