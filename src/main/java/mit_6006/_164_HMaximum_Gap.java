581972255049c093d1e432044e824ad11c2126a9
softtwilight <tjuyxl@gmail.com>
2020-10-30 22:11:06 +0900

    bucket sort for max gap

--- src/main/java/mit_6006/_164_HMaximum_Gap.java
+++ src/main/java/mit_6006/_164_HMaximum_Gap.java
@@ -34,7 +34,11 @@ public class _164_HMaximum_Gap {
         ArrayList<Integer>[] buckets = new ArrayList[di];
         for (int k = 0; k < di; k++) buckets[k] = new ArrayList();
 
-        for (int i = 1; i <= 1000000000; i *= di) {
+        int max = 0;
+        for (int n : nums) {
+            max = Math.max(max, n);
+        }
+        for (int i = 1; i <= max; i <<= di) {
             for (int j = 0; j < nums.length; j++) {
                 buckets[(nums[j] / i) % di].add(nums[j]);
             }
@@ -46,11 +50,49 @@ public class _164_HMaximum_Gap {
                 bucket.clear();
             }
         }
-        int max = 0;
+        max = 0;
         for (int i = 1; i < nums.length; i++) {
             max = Math.max(max, nums[i] - nums[i - 1]);
         }
         return max;
+    }
+
+    /**
+     * bucket sort.
+     * 关键的点是这样的。
+     * 找到max， min
+     * 然后最平均分配的情况下，gap = （max - min) / n - 1;
+     * 那么我们用gap大小当bucket， 在桶内的元素最大gap是小于gap的
+     * 所以gap一定是存在于bucket 和 bucket 之间。
+     * 这时候我们找最大值，只需要找之前的gap就行了。
+     */
+    public int maximumGap2(int[] nums) {
+        int n = nums.length;
+        if(n < 2) return 0;
+        int min = nums[0];
+        int max = nums[0];
+        for(int i = 1;i < n;i++){
+            if(min > nums[i]) min = nums[i];
+            if(max < nums[i]) max = nums[i];
+        }
+
+        int gap = (max-min)/(n-1);
+        if(gap == 0) gap++;
+        int len = (max-min)/gap+1;
+        int [] tmax = new int [len];
+        int [] tmin = new int [len];
+
+        for(int i = 0;i < n;i++){
+            int index = (nums[i]-min)/gap;
+            if(nums[i] > tmax[index]) tmax[index] = nums[i];
+            if(tmin[index] == 0 || nums[i] < tmin[index]) tmin[index] = nums[i];
+        }
+        int myMax = 0;
+        for(int i = 0;i < len;i++){
+            if(myMax < tmin[i]-min) myMax = tmin[i]-min;
+            if(tmax[i] != 0) min = tmax[i];
+        }
+        return myMax;
     }
 
 }
