7844be0c53cfe24d467c124428ddbef41c1af82f
softtwilight <tjuyxl@gmail.com>
2020-12-17 20:54:36 +0900

    优化dfs，标记在visiting 和 visited

--- src/main/java/mit_6006/_207_M_Course_Schedule.java
+++ src/main/java/mit_6006/_207_M_Course_Schedule.java
@@ -24,7 +24,7 @@ public class _207_M_Course_Schedule {
      * 这道题其实是cycle detect， 用boolean值有个缺陷，有个遍历节点还会在遍历一遍
      * 看到有用int 作为数组， 然后标记0， 1， 2 三种状态，然后visiting 标记为1， visited标记为2
      */
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
+    public boolean canFinish2(int numCourses, int[][] prerequisites) {
         //init
         List<List<Integer>> adjacency = new ArrayList<>(numCourses);
         for (int i = 0; i < numCourses; i++) {
@@ -37,6 +37,39 @@ public class _207_M_Course_Schedule {
         boolean[] traveled = new boolean[numCourses];
         for (int i = 0; i < numCourses ; i++) {
 
+            if (!dfs2(i, adjacency, traveled)) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    private boolean dfs2(int n, List<List<Integer>> adjacency,  boolean[] traveled) {
+        if (traveled[n] == true) return false;
+        traveled[n] = true;
+        for (int i : adjacency.get(n)) {
+            if (!dfs2(i, adjacency, traveled)) {
+                return false;
+            }
+        }
+        traveled[n] = false;
+        return true;
+    }
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        //init
+        List<List<Integer>> adjacency = new ArrayList<>(numCourses);
+        for (int i = 0; i < numCourses; i++) {
+            adjacency.add(new ArrayList<>());
+        }
+        for(int[] edge : prerequisites) {
+            adjacency.get(edge[1]).add(edge[0]);
+        }
+
+        int[] traveled = new int[numCourses];
+        for (int i = 0; i < numCourses ; i++) {
+
             if (!dfs(i, adjacency, traveled)) {
                 return false;
             }
@@ -45,15 +78,17 @@ public class _207_M_Course_Schedule {
 
     }
 
-    private boolean dfs(int n, List<List<Integer>> adjacency,  boolean[] traveled) {
-        if (traveled[n] == true) return false;
-        traveled[n] = true;
+    private boolean dfs(int n, List<List<Integer>> adjacency,  int[] traveled) {
+        if (traveled[n] == 1) return false;
+        traveled[n] = 1;
         for (int i : adjacency.get(n)) {
-            if (!dfs(i, adjacency, traveled)) {
-                return false;
+            if (traveled[i] != 2) {
+                if (!dfs(i, adjacency, traveled)) {
+                    return false;
+                }
             }
         }
-        traveled[n] = false;
+        traveled[n] = 2;
         return true;
     }
 }
