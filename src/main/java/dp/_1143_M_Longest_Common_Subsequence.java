59dc453a547ce7f971c936dca47a7fe7fbedfb27
softtwilight <tjuyxl@gmail.com>
2020-12-22 00:14:47 +0900

    bottom-up solution

--- src/main/java/dp/_1143_M_Longest_Common_Subsequence.java
+++ src/main/java/dp/_1143_M_Longest_Common_Subsequence.java
@@ -46,4 +46,25 @@ public class _1143_M_Longest_Common_Subsequence {
         memo[i][j] = result;
         return result;
     }
+
+    /**
+     * bottom - up.
+     */
+    public int longestCommonSubsequence2(String text1, String text2) {
+        int n = text1.length();
+        int m = text2.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(text1.charAt(i - 1) == text2.charAt(j - 1))
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                else
+                    dp[i][j] = Math.max(dp[i - 1][j],dp[i][j - 1]);
+            }
+        }
+
+        return dp[n][m];
+    }
 }
