2945f3e35fa442b1983b94e36d1c7310bb83d571
softtwilight <tjuyxl@gmail.com>
2021-01-12 23:37:17 +0900

    simplify two loop to one loop

--- src/main/java/array/_1124_M_Longest_WellPerforming_Interval.java
+++ src/main/java/array/_1124_M_Longest_WellPerforming_Interval.java
@@ -15,38 +15,29 @@ public class _1124_M_Longest_WellPerforming_Interval {
     private static final _1124_M_Longest_WellPerforming_Interval instance = new _1124_M_Longest_WellPerforming_Interval();
 
     public static void main(String[] args) {
-        int[] hours = {-9,6,6};
+        int[] hours = {9,6,6};
         System.out.println(instance.longestWPI(hours));
     }
 
     /**
-     *
+     * 两个循环简化为一个循环
      */
     public int longestWPI(int[] hours) {
         int result = 0;
-        int[] prefixSum = new int[hours.length + 1];
+        int stack = 0;
         Map<Integer, Integer> indexCache = new HashMap<>();
-        indexCache.put(0, 0);
         for (int i = 0; i < hours.length; i++) {
-            if (hours[i] > 8) {
-                prefixSum[i + 1] = prefixSum[i] + 1;
+            stack += hours[i] > 8 ? 1 : -1;
+            if (stack > 0) {
+                result = i + 1;
             } else {
-                prefixSum[i + 1] = prefixSum[i] - 1;
-            }
-            if (!indexCache.containsKey(prefixSum[i + 1])) {
-                indexCache.put(prefixSum[i + 1], i + 1);
-            }
-        }
-//        System.out.println(Arrays.toString(prefixSum));
-        for (int i = 0; i < prefixSum.length; i++) {
-            if (prefixSum[i] > 0) {
-                result = Math.max(result, i);
-            } else {
-                if (indexCache.containsKey(prefixSum[i] - 1)) {
-                    int lo = indexCache.get(prefixSum[i] - 1);
-                    result = Math.max(result, i - lo);
+                indexCache.putIfAbsent(stack, i);
+                if (indexCache.containsKey(stack - 1)) {
+                    result = Math.max(result, i - indexCache.get(stack - 1));
                 }
             }
+
+
         }
         return result;
     }
