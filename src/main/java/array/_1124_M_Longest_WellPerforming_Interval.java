e1053e8e4b7ebe5265688e41f66294e614f88a7c
softtwilight <tjuyxl@gmail.com>
2021-01-13 19:16:11 +0900

    more general approach to this kind of problem. the longest the sub array whose sum is under a condition

--- src/main/java/array/_1124_M_Longest_WellPerforming_Interval.java
+++ src/main/java/array/_1124_M_Longest_WellPerforming_Interval.java
@@ -1,9 +1,6 @@
 package array;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Author:   softtwilight
@@ -15,14 +12,47 @@ public class _1124_M_Longest_WellPerforming_Interval {
     private static final _1124_M_Longest_WellPerforming_Interval instance = new _1124_M_Longest_WellPerforming_Interval();
 
     public static void main(String[] args) {
-        int[] hours = {9,6,6};
+        int[] hours = {9,9,6,0,6,6,9};
         System.out.println(instance.longestWPI(hours));
+    }
+
+
+    /**
+     * a more general approach to solve this kind of problem.
+     *
+     * input: arrary arr in which elements are arbitrary integers.
+     * output: length of a longest subarray arr[i, j) with sum(arr[i], ... , arr[j-1]) >= K.
+     *
+     */
+    public int longestWPI(int[] hours) {
+        int[] prefixSum = new int[hours.length + 1];
+        for (int i = 1; i <= hours.length; i++) {
+            prefixSum[i] = prefixSum[i - 1] + (hours[i - 1] > 8 ? 1 : - 1);
+        }
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < prefixSum.length; i++) {
+            if (stack.isEmpty() || prefixSum[stack.peek()] > prefixSum[i]) {
+                stack.push(i);
+            }
+        }
+
+        int result = 0;
+        System.out.println(Arrays.toString(prefixSum));
+        System.out.println(stack);
+        for (int i = prefixSum.length - 1; i >= 0 ; i--) {
+            while (!stack.isEmpty() && prefixSum[i] > prefixSum[stack.peek()]) {
+                result = Math.max(result, i - stack.pop());
+            }
+        }
+
+        return result;
+
     }
 
     /**
      * 两个循环简化为一个循环
      */
-    public int longestWPI(int[] hours) {
+    public int longestWPI2(int[] hours) {
         int result = 0;
         int stack = 0;
         Map<Integer, Integer> indexCache = new HashMap<>();
