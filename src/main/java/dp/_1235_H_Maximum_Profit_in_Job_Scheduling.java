59a8c0dbcac8f1f663b192b2ecce910900ab8f08
softtwilight <tjuyxl@gmail.com>
2021-01-20 22:26:50 +0900

    a more efficient way to recursive. n * log n

--- src/main/java/dp/_1235_H_Maximum_Profit_in_Job_Scheduling.java
+++ src/main/java/dp/_1235_H_Maximum_Profit_in_Job_Scheduling.java
@@ -16,7 +16,54 @@ public class _1235_H_Maximum_Profit_in_Job_Scheduling {
         int[] startTime = {4,2,4,8,2};
         int[] endTime = {5,5,5,10,8};
         int[] profit = {1,2,8,10,4};
+        System.out.println(instance.jobScheduling2(startTime, endTime, profit));
         System.out.println(instance.jobScheduling(startTime, endTime, profit));
+    }
+
+
+    /**
+     * more efficient way to dp.
+     * need not compare n result, just compare two.
+     */
+    public int jobScheduling2(int[] startTime, int[] endTime, int[] profit) {
+        jobs = new Job[startTime.length];
+        for (int i = 0; i < startTime.length; i++) {
+            jobs[i] = new Job(startTime[i], endTime[i], profit[i]);
+        }
+        Arrays.sort(jobs, Comparator.comparingInt(j -> j.startAt));
+        int[] memo = new int[profit.length];
+        return dp2(0, memo);
+    }
+
+    private int dp2(int lo, int[] memo) {
+        if (lo >= memo.length) return 0;
+        if (memo[lo] != 0) return memo[lo];
+        int takeLo = jobs[lo].pro + dp2(findNext(lo, jobs[lo].endAt), memo);
+        int notTakeLo = dp2(lo + 1, memo);
+        memo[lo] = Math.max(takeLo, notTakeLo);
+        return memo[lo];
+    }
+
+    private int findNext(int lo, int endAt) {
+        for (int i = lo + 1; i < jobs.length; i++) {
+            if (jobs[i].startAt >= endAt) {
+                return i;
+            }
+        }
+        return  jobs.length;
+    }
+
+
+    class Job {
+        int startAt;
+        int endAt;
+        int pro;
+
+        public Job(int startAt, int endAt, int pro) {
+            this.startAt = startAt;
+            this.endAt = endAt;
+            this.pro = pro;
+        }
     }
 
     /**
@@ -33,17 +80,7 @@ public class _1235_H_Maximum_Profit_in_Job_Scheduling {
         return dp(0, memo);
     }
 
-    class Job {
-        int startAt;
-        int endAt;
-        int pro;
 
-        public Job(int startAt, int endAt, int pro) {
-            this.startAt = startAt;
-            this.endAt = endAt;
-            this.pro = pro;
-        }
-    }
 
     Job[] jobs;
     private int dp(int lo, int[] memo) {
